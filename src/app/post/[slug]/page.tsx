a151742a03c20e9224c2daf63d4c7ed0b73dc9fa
Onion-L <lixiang020317@gmail.com>
2024-10-22 14:35:50 +0800

    fix: image loading error

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -14,14 +14,21 @@ export default function DynamicPage({ params }: { params: { slug: string } }) {
   const { content } = getDocByName(slug, postDirectory)
 
   return (
-    <div>
+    <div className="flex flex-col justify-center w-full">
+      <div className="w-full h-4"></div>
       <ReactMarkdown
         components={{
           img: ({ src, alt }) => (
-            <Image src={src || ''} alt={alt || ''} width={500} height={300} />
+            <Image
+              className="mx-auto"
+              src={src || ''}
+              alt={alt || ''}
+              width={600}
+              height={400}
+            />
           )
         }}
-        className="prose prose-invert"
+        className="prose prose-invert max-w-2xl w-full mx-auto"
       >
         {content}
       </ReactMarkdown>
