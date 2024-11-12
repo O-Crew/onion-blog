320226aaf98448471a0988d598c8067780364d73
Onion-L <lixiang020317@gmail.com>
2024-11-12 17:14:27 +0800

    feat: code syntax highlight in markdown

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,5 +1,7 @@
 import { getPostList, getDocByName, postDirectory } from '@/utils/docs'
 import ReactMarkdown from 'react-markdown'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism'
 import Image from 'next/image'
 
 export async function generateStaticParams() {
@@ -8,7 +10,7 @@ export async function generateStaticParams() {
     slug: post.path.replace('.md', '')
   }))
 }
-// TODO code highlight
+// TODO replace highlighter with shiki.js
 export default function DynamicPage({ params }: { params: { slug: string } }) {
   const slug = params.slug
   const { content } = getDocByName(slug, postDirectory)
@@ -32,6 +34,17 @@ export default function DynamicPage({ params }: { params: { slug: string } }) {
                 height={400}
               />
             )
+          },
+          code: ({ className, children }) => {
+            if (className?.startsWith('language-')) {
+              const language = className?.split('-')[1].toLowerCase()
+              return (
+                <SyntaxHighlighter language={language} style={vscDarkPlus}>
+                  {String(children).replace(/\n$/, '')}
+                </SyntaxHighlighter>
+              )
+            }
+            return <code className={className}>{children}</code>
           }
         }}
         className="prose prose-invert max-w-2xl w-full mx-auto"
@@ -41,3 +54,47 @@ export default function DynamicPage({ params }: { params: { slug: string } }) {
     </div>
   )
 }
+
+// {
+//   className: 'language-TypeScript',
+//   node: {
+//     type: 'element',
+//     tagName: 'code',
+//     properties: { className: [Array] },
+//     children: [ [Object] ],
+//     position: { start: [Object], end: [Object] }
+//   },
+//   children: '    const cleanup = () => {\r\n' +
+//     '    cleanups.forEach(fn => fn())\r\n' +
+//     '    cleanups.length = 0\r\n' +
+//     '  }\r\n' +
+//     ' \r\n' +
+//     '  const register =\r\n' +
+//     '  (el: any, event: string, listener: any, options: any) => {\r\n' +
+//     '    el.addEventListener(event, listener, options)\r\n' +
+//     '    return () => el.removeEventListener(event, listener, options)\r\n' +
+//     '  }\r\n' +
+//     '\r\n' +
+//     '  const stopWatch = watch(\r\n' +
+//     '    () => [unrefElement(target as unknown as MaybeElementRef),toValue(options)],\r\n' +
+//     '    ([el, options]) => {\r\n' +
+//     '      cleanup()\r\n' +
+//     '      if (!el)\r\n' +
+//     '        return\r\n' +
+//     '      const optionsClone = isObject(options) ? { ...options } : options\r\n' +
+//     '      cleanups.push(\r\n' +
+//     '        ...(events as string[]).flatMap((event) => {\r\n' +
+//     '    return (listeners as Function[])\r\n' +
+//     '        .map(listener => register(el, event, listener, optionsClone))\r\n' +
+//     '        }),\r\n' +
+//     '      )\r\n' +
+//     '    },\r\n' +
+//     "    { immediate: true, flush: 'post' },\r\n" +
+//     '  )\r\n' +
+//     ' \r\n' +
+//     '  const stop = () => {\r\n' +
+//     '    stopWatch()\r\n' +
+//     '    cleanup()\r\n' +
+//     '  }\r\n' +
+//     '  tryOnScopeDispose(stop)\n'
+// }
